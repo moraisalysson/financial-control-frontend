bddfd96a7e36628ae090066a31341710e9add36f
alysson.morais <alysson.morais@dataprev.gov.br>
2024-12-31 17:13:16 -0300

    adding routes

--- financial-control/src/app/app-routing.module.ts
+++ financial-control/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContasBancariasModule } from './contas-bancarias/contas-bancarias.module';
+import { ContasFormComponent } from './contas-bancarias/contas-form/contas-form.component';
+import { ContasListComponent } from './contas-bancarias/contas-list/contas-list.component';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'contas-bancarias'},
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path: 'contas-bancarias/form', component: ContasFormComponent},
+  {path: 'contas-bancarias/list', component: ContasListComponent},
   {
-    path: 'contas-bancarias',
-    loadChildren: () => import('./contas-bancarias/contas-bancarias.module').then(m => m.ContasBancariasModule)
+    path: 'home',
+    loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
   }
 ];
 
